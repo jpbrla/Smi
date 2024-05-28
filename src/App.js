f512073749e08167ddc8c478586313f793db4f31
jpbrla <jeremyparkerbobrick@gmail.com>
2024-05-28 09:45:02 -0700

    analytics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import "./App.scss";
 
 import Navbar from "./components/Navbar";
