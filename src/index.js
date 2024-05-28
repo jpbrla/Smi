d300af3b05fdaff13d0f25db65a75a6a731545d7
jpbrla <jeremyparkerbobrick@gmail.com>
2024-05-28 09:18:09 -0700

    adding analytics 2

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Analytics } from "@vercel/analytics/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
