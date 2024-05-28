d78558b3588f382443e6d7ca0f388e78a69f689a
jpbrla <jeremyparkerbobrick@gmail.com>
2024-05-28 08:36:09 -0700

    updating footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Footer = () => {
     return (
-        <p className="text-center fc-primary mt-30">Footer copyright text</p>
+        <p className="text-center fc-primary mt-30">&copy; 2024 Bobrick Washroom Equipment, Inc. All rights reserved</p>
     )
 }
 
