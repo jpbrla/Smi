b7735f9511d7453e647a0fa87ab4dbc19a64212a
jpbrla <167252042+jpbrla@users.noreply.github.com>
2024-06-05 13:39:40 -0700

    trying hr again

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,13 @@
 import React from "react"
 import Divider from '@mui/material/Divider'
 
+<Divider />
+
 const Footer = () => {
     return (
+
         <p className="text-center fc-primary mt-30">&copy; 2024 Bobrick Washroom Equipment, Inc.</p>
     )
 }
-
-
-<hr class="MuiDivider-root">
-<Divider />
-</hr>
 
 export default Footer
