e4005f6ee2c59a3a2cb513582e7f6607077a19ca
Jeremy Parker <Jeremy.Parker@bobrick.net>
2024-06-20 16:00:40 -0700

    updating report page

--- src/pages/status_report/index.js
+++ src/pages/status_report/index.js
@@ -36,72 +36,72 @@ const StatusReportPage = () => {
                     <p className="fs-16 w-60 f-regular fc-darkgrey">Example title about automating the upgrading of floor panels</p>
                 </div>
                 <div className="d-flex">
-                    <p className="fs-14 w-40 text-end pr-10 f-bold fc-black">Originator</p>
-                    <p className="fs-14 w-60 f-regular fc-darkgrey">Sample Originatorname</p>
+                    <p className="fs-16 w-40 text-end pr-10 f-bold fc-black">Originator</p>
+                    <p className="fs-16 w-60 f-regular fc-darkgrey">Sample Originatorname</p>
                 </div>
                 <div className="d-flex">
-                    <p className="fs-14 w-40 text-end pr-10 f-bold fc-black">Additional Originator(s)</p>
-                    <p className="fs-14 w-60 f-regular fc-darkgrey">Firstname Lastname, Originator Nametwo</p>
+                    <p className="fs-16 w-40 text-end pr-10 f-bold fc-black">Additional Originator(s)</p>
+                    <p className="fs-16 w-60 f-regular fc-darkgrey">Firstname Lastname, Originator Nametwo</p>
                 </div>
                 <div className="d-flex">
-                    <p className="fs-14 w-40 text-end pr-10 f-bold fc-black">Owner</p>
-                    <p className="fs-14 w-60 f-regular fc-darkgrey">Sample Ownername</p>
+                    <p className="fs-16 w-40 text-end pr-10 f-bold fc-black">Owner</p>
+                    <p className="fs-16 w-60 f-regular fc-darkgrey">Sample Ownername</p>
                 </div>
                 <div className="d-flex">
-                    <p className="fs-14 w-40 text-end pr-10 f-bold fc-black">Assigned to Owner</p>
-                    <p className="fs-14 w-60 f-regular fc-darkgrey">02/02/24</p>
+                    <p className="fs-16 w-40 text-end pr-10 f-bold fc-black">Assigned to Owner</p>
+                    <p className="fs-16 w-60 f-regular fc-darkgrey">02/02/24</p>
                 </div>
                 <div className="d-flex">
-                    <p className="fs-14 w-40 text-end pr-10 f-bold fc-black">Division</p>
-                    <p className="fs-14 w-60 f-regular fc-darkgrey">BLA</p>
+                    <p className="fs-16 w-40 text-end pr-10 f-bold fc-black">Division</p>
+                    <p className="fs-16 w-60 f-regular fc-darkgrey">BLA</p>
                 </div>
                 <div className="d-flex">
-                    <p className="fs-14 w-40 text-end pr-10 f-bold fc-black">Department</p>
-                    <p className="fs-14 w-60 f-regular fc-darkgrey">Office Services</p>
+                    <p className="fs-16 w-40 text-end pr-10 f-bold fc-black">Department</p>
+                    <p className="fs-16 w-60 f-regular fc-darkgrey">Office Services</p>
                 </div>
                 <div className="d-flex">
-                    <p className="fs-14 w-40 text-end pr-10 f-bold fc-black">Improvement Area</p>
-                    <p className="fs-14 w-60 f-regular fc-darkgrey">Automation</p>
+                    <p className="fs-16 w-40 text-end pr-10 f-bold fc-black">Improvement Area</p>
+                    <p className="fs-16 w-60 f-regular fc-darkgrey">Automation</p>
                 </div>
                 <div className="d-flex">
-                    <p className="fs-14 w-40 text-end pr-10 f-bold fc-black">Description</p>
-                    <p className="fs-14 w-60 f-regular fc-darkgrey">Set up notice to upgrade floor panels every 7 months. Just another sentence to showcase full area of description field.</p>
+                    <p className="fs-16 w-40 text-end pr-10 f-bold fc-black">Description</p>
+                    <p className="fs-16 w-60 f-regular fc-darkgrey">Set up notice to upgrade floor panels every 7 months. Just another sentence to showcase full area of description field.</p>
                 </div><br></br>
                 <div className="d-flex">
-                    <p className="fs-14 w-40 text-end pr-10 f-bold fc-black">Images</p>
+                    <p className="fs-16 w-40 text-end pr-10 f-bold fc-black">Images</p>
                     <div className="w-60 d-flex flex-column">
-                        <p className="fs-14 w-100 f-regular-italic fc-primary">1. image001.png</p>
-                        <p className="fs-14 w-100 f-regular-italic fc-primary">2. image002.png</p>
+                        <p className="fs-16 w-100 f-regular-italic fc-primary">1. image001.png</p>
+                        <p className="fs-16 w-100 f-regular-italic fc-primary">2. image002.png</p>
                     </div>
                 </div><br></br>
                 <div className="d-flex">
-                    <p className="fs-14 w-40 text-end pr-10 f-bold fc-black">Additional Documents</p>
+                    <p className="fs-16 w-40 text-end pr-10 f-bold fc-black">Additional Documents</p>
                     <div className="w-60 d-flex flex-column">
-                        <p className="fs-14 w-100 f-regular-italic fc-primary">1. audiofile.mp3</p>
-                        <p className="fs-14 w-100 f-regular-italic fc-primary">2. videofile.mp4</p>
-                        <p className="fs-14 w-100 f-regular-italic fc-primary">3. worddoc.docx</p>
-                        <p className="fs-14 w-100 f-regular-italic fc-primary">4. excel.xlsx</p>
+                        <p className="fs-16 w-100 f-regular-italic fc-primary">1. audiofile.mp3</p>
+                        <p className="fs-16 w-100 f-regular-italic fc-primary">2. videofile.mp4</p>
+                        <p className="fs-16 w-100 f-regular-italic fc-primary">3. worddoc.docx</p>
+                        <p className="fs-16 w-100 f-regular-italic fc-primary">4. excel.xlsx</p>
                     </div>
                 </div><br></br>
                 <div className="d-flex">
-                    <p className="fs-14 w-40 text-end pr-10 f-bold fc-black">Annual Cost Savings</p>
-                    <p className="fs-14 w-60 f-bold fc-green">$1,234.56</p>
+                    <p className="fs-16 w-40 text-end pr-10 f-bold fc-black">Annual Cost Savings</p>
+                    <p className="fs-16 w-60 f-bold fc-green">$1,234.56</p>
                 </div><br></br>
                 <div className="d-flex">
-                    <p className="fs-14 w-40 text-end pr-10 f-bold fc-black">Notifications</p>
+                    <p className="fs-16 w-40 text-end pr-10 f-bold fc-black">Notifications</p>
                     <div className="w-60 d-flex flex-column">
-                        <p className="fs-14 f-regular-italic fc-darkgrey">Upcoming Dates</p>
+                        <p className="fs-16 f-regular-italic fc-darkgrey">Upcoming Dates</p>
                         <div className="d-flex mt-1">
-                            <span className="fs-14 fc-white bg-green px-1">30 Day</span>
-                            <span className="fs-14 ml-10 fc-grey">02/29/24</span>
+                            <span className="fs-16 fc-white bg-green px-1">30 Day</span>
+                            <span className="fs-16 ml-10 fc-grey">02/29/24</span>
                         </div>
                         <div className="d-flex mt-1">
-                            <span className="fs-14 fc-white bg-orange px-1">60 Day</span>
-                            <span className="fs-14 ml-10 fc-grey">03/30/24</span>
+                            <span className="fs-16 fc-white bg-orange px-1">60 Day</span>
+                            <span className="fs-16 ml-10 fc-grey">03/30/24</span>
                         </div>
                         <div className="d-flex mt-1">
-                            <span className="fs-14 fc-white bg-red px-1">90 Day</span>
-                            <span className="fs-14 ml-10 fc-grey">04/30/24</span>
+                            <span className="fs-16 fc-white bg-red px-1">90 Day</span>
+                            <span className="fs-16 ml-10 fc-grey">04/30/24</span>
                         </div>
                     </div>
                 </div>
@@ -171,20 +171,20 @@ const StatusReportPage = () => {
             </div>
             <div className="d-flex flex-column mt-20">
                 <div className="d-flex">
-                    <p className="fs-14 w-40 text-end pr-10 f-regular fc-darkgrey">02/02/24</p>
-                    <p className="fs-14 w-60 f-regular fc-darkgrey">Assigned to Owner</p>
+                    <p className="fs-16 w-40 text-end pr-10 f-regular fc-darkgrey">02/02/24</p>
+                    <p className="fs-16 w-60 f-regular fc-darkgrey">Assigned to Owner</p>
                 </div>
                 <div className="d-flex">
-                    <p className="fs-14 w-40 text-end pr-10 f-regular fc-darkgrey">02/03/24</p>
-                    <p className="fs-14 w-60 f-regular fc-darkgrey">Due to Implementation</p>
+                    <p className="fs-16 w-40 text-end pr-10 f-regular fc-darkgrey">02/03/24</p>
+                    <p className="fs-16 w-60 f-regular fc-darkgrey">Due to Implementation</p>
                 </div>
                 <div className="d-flex">
-                    <p className="fs-14 w-40 text-end pr-10 f-regular fc-darkgrey">02/04/24</p>
-                    <p className="fs-14 w-60 f-regular fc-darkgrey">Implemented</p>
+                    <p className="fs-16 w-40 text-end pr-10 f-regular fc-darkgrey">02/04/24</p>
+                    <p className="fs-16 w-60 f-regular fc-darkgrey">Implemented</p>
                 </div>
                 <div className="d-flex">
-                    <p className="fs-14 w-40 text-end pr-10 f-regular fc-darkgrey">02/05/24</p>
-                    <p className="fs-14 w-60 f-regular fc-darkgrey">Closed</p>
+                    <p className="fs-16 w-40 text-end pr-10 f-regular fc-darkgrey">02/05/24</p>
+                    <p className="fs-16 w-60 f-regular fc-darkgrey">Closed</p>
                 </div>
             </div>
         </div>
