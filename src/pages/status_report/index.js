c4a2d5680b94cac18093eebebb1a1b9037d232b6
Jeremy Parker <Jeremy.Parker@bobrick.net>
2024-06-20 16:09:58 -0700

    font weights

--- src/pages/status_report/index.js
+++ src/pages/status_report/index.js
@@ -126,7 +126,7 @@ const StatusReportPage = () => {
                     </ul>
                 </div>
                 
-                <div className={"w-100" + (stageUpdateNote==="" ? " border-red" : " border-green")}>
+                <div className={"w-75" + (stageUpdateNote==="" ? " border-red" : " border-green")}>
                 <p className="w-50 py-10 bg-lightgrey text-center fs-20 mt-20">Status Update Notes</p>
                     <div className="d-flex bg-lightgrey">
                         <div className="d-flex flex-column align-items-center w-50">
