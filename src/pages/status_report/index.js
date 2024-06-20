0fde36f3d83f4d4df46472549ac0f24ed82aa246
Jeremy Parker <Jeremy.Parker@bobrick.net>
2024-06-20 16:03:24 -0700

    Update index.js

--- src/pages/status_report/index.js
+++ src/pages/status_report/index.js
@@ -125,8 +125,9 @@ const StatusReportPage = () => {
                         }
                     </ul>
                 </div>
-                <p className="w-50 py-10 bg-lightgrey text-center fs-20 mt-20">Status Update Notes</p>
+                
                 <div className={"w-100" + (stageUpdateNote==="" ? " border-red" : " border-green")}>
+                <p className="w-50 py-10 bg-lightgrey text-center fs-20 mt-20">Status Update Notes</p>
                     <div className="d-flex bg-lightgrey">
                         <div className="d-flex flex-column align-items-center w-50">
                             <p className="fs-16">Text Options</p>
