a1925ea3423b99a1ff770a1d91df951f441ca690
Jeremy Parker <Jeremy.Parker@bobrick.net>
2024-06-20 16:27:44 -0700

    Update index.js

--- src/pages/status_report/index.js
+++ src/pages/status_report/index.js
@@ -110,7 +110,7 @@ const StatusReportPage = () => {
                 <p className="w-50 py-10 bg-lightgrey text-center fs-20">Change SMI Status</p>
             </div>
             <div className={"d-flex flex-column align-items-center"}>
-                <div className="d-flex flex-column w-75 bordered-1 border-grey mt-20">
+                <div className="d-flex flex-column w-75 bordered-1 border-grey">
                     <div className="d-flex justify-content-around bg-lightgrey py-3 w-100"></div>
                     <ul className="menus w-100">
                         {
